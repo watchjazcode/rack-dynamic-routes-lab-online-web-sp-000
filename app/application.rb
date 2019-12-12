6c964ab5c4aa591978a714765858dc5ddfe9addf
Jazmin M <jazminmerckel@gmail.com>
2019-12-12 07:07:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,6 +15,7 @@ class Application
         resp.status = 400
       else
         resp.write "#{chosen_item.price}"
+      end
     else
       resp.write "Route not found"
       resp.status = 404
