4591ea665046ff57a0da0dcaf3da4d55dec2ccb7
Jazmin M <jazminmerckel@gmail.com>
2019-12-12 07:07:16 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,26 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    #get '/items/Figs'
+    #get '/items/Apples'
+
+    if req.path.start_with? "/items"
+      item = req.path.split("/").last
+      chosen_item = @@items.detect { |i| i.name == item }
+      if chosen_item.nil?
+        resp.write "Item not found"
+        resp.status = 400
+      else
+        resp.write "#{chosen_item.price}"
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+end
